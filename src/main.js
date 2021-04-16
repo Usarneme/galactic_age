35a34d183a31fe3aaf9872d936ad4a47e83c35d1
Usarneme <jamestcoop@gmail.com>
2021-04-16 14:20:01 -0700

    adds containers for the planets and builds out the layout some more

--- src/main.js
+++ src/main.js
@@ -5,3 +5,11 @@ import './css/styles.css'
 
 // $("section").text("TEST TEXT INPUT")
 console.log($)
+let age
+
+$("#datepicker").on("change", event => {
+  console.log("date change",event.target.valueAsNumber)
+  age = (Date.now() - event.target.valueAsNumber) / 1000 / 60 / 60 / 24 / 365.25
+  console.log(age)
+  $(".age").text(`You are approximately ${age.toFixed(2)} years old on Earth.`).show()
+})
