21b4b88c0330094116430a30b47df25d2d395173
Usarneme <jamestcoop@gmail.com>
2021-04-16 08:16:08 -0700

    adds a test for constructing a new galactic age instance.

--- __tests__/GalacticAge.test.js
+++ __tests__/GalacticAge.test.js
@@ -1,5 +1,10 @@
-describe("TEMPLATE TEST", () => {
+describe("Galactic Age", () => {
   test("sanity check", () => {
     expect(true).toEqual(true)
+  })
+
+  test("GalacticAge class should construct an object", () => {
+    const myGalacticAge = new GalacticAge()
+    expect(myGalacticAge).toBeDefined()
   })
 })
