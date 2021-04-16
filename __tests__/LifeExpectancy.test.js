4062128aa76ba114b7267c585d4d2a07458dc92e
Usarneme <jamestcoop@gmail.com>
2021-04-16 10:40:16 -0700

    refactors lifeexpectancy to use a beforeEach func to populate data used in tests

--- __tests__/LifeExpectancy.test.js
+++ __tests__/LifeExpectancy.test.js
@@ -4,15 +4,19 @@ describe("LifeExpectancy", () => {
   test("sanity check", () => {
     expect(true).toEqual(true)
   })
+  const age = 15
+  let myLifeExpectancy
+
+  beforeEach(() => {
+    myLifeExpectancy = new LifeExpectancy(age)
+  })
+
 
   test("LifeExpectancy class should exist", () => {
-    const myLifeExpectancy = new LifeExpectancy()
     expect(myLifeExpectancy).toBeDefined()
   })
 
   test("LifeExpectancy should take in an age and store it", () => {
-    const age = 15
-    const myLifeExpectancy = new LifeExpectancy(age)
     expect(myLifeExpectancy.age).toEqual(age)
   })
 })
