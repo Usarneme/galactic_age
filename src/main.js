3dbf4250f3208f87971b5617fa980bfc9b5c0410
Usarneme <jamestcoop@gmail.com>
2021-04-16 15:07:46 -0700

    adds validation and a warning if you have not entered dob when clicking on a planet

--- src/main.js
+++ src/main.js
@@ -6,12 +6,16 @@ import GalacticAge from './GalacticAge'
 
 let age
 let myGalacticAge
+
+// helper functions
 function updateAge(event) {
   age = (Date.now() - event.target.valueAsNumber) / 1000 / 60 / 60 / 24 / 365.25
   myGalacticAge = new GalacticAge(age)
+  $(".warning").hide()
   $(".age").text(`You are approximately ${myGalacticAge.earthAge.toFixed(2)} years old on Earth.`).show()
 }
 
+// event listeners
 $("#datepicker").on("change", event => {
   if (event && event.target && event.target.valueAsNumber && !isNaN(event.target.valueAsNumber)) updateAge(event)
 })
@@ -22,4 +26,25 @@ $("#datepicker").on("blur", event => {
 
 $("#datepicker").on("focus", event => {
   if (event && event.target && event.target.valueAsNumber && !isNaN(event.target.valueAsNumber)) updateAge(event)
+})
+
+$("#close-warning").on("click", () => {
+  $(".warning").hide()
+})
+
+$(".planets").on("click", "div", function(event) {
+  event.stopPropagation()
+  if (age === undefined) return $(".warning").show()
+  if (this.classList[0] === "jupiter") {
+    console.log("jupiter")
+  }
+  if (this.classList[0] === "venus") {
+    console.log("venus")
+  }
+  if (this.classList[0] === "mercury") {
+    console.log("mercury")
+  }
+  if (this.classList[0] === "mars") {
+    console.log("mars")
+  }
 })
