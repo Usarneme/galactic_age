44646cd41ad80fab6adb6ea42d42d0eaf1340599
Usarneme <jamestcoop@gmail.com>
2021-04-16 10:32:49 -0700

    adds a failing test expecting LifeExpectancy class to exist

--- __tests__/LifeExpectancy.test.js
+++ __tests__/LifeExpectancy.test.js
@@ -3,4 +3,9 @@ describe("LifeExpectancy", () => {
   test("sanity check", () => {
     expect(true).toEqual(true)
   })
+
+  test("LifeExpectancy class should exist", () => {
+    const myLifeExpectancy = new LifeExpectancy()
+    expect(myLifeExpectancy).toBeDefined()
+  })
 })
