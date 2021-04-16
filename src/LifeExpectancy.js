d9772cbfd385e129e8efa1153ef91f3336b91e16
Usarneme <jamestcoop@gmail.com>
2021-04-16 10:50:19 -0700

    passes the previous test by importing the yearsRemaining data into a variable and updating the yearsRemaining method

--- src/LifeExpectancy.js
+++ src/LifeExpectancy.js
@@ -1,9 +1,11 @@
+const yearsRemainingTable = require('./lifeExpectancy.json')
+
 export default class LifeExpectancy {
   constructor(age) {
     this.age = age
   }
 
   yearsRemaining() {
-    
+    return yearsRemainingTable[this.age]
   }
 }
